30ff9f6bfaa94de10580cd48678a0fcac4327d94
Aamir Suleman <asuleman@blueraster.com>
2014-06-24 16:37:53 -0400

    footer updates

--- src/app/js/views/footer/FooterController.js
+++ src/app/js/views/footer/FooterController.js
@@ -20,12 +20,50 @@ define(["dojo/dom", "dijit/registry", "modules/HashController", "modules/EventsC
 
                 dom.byId(viewName).innerHTML = html;
 
+                that.initShareButton()
+
                 FooterModel.applyBindings(viewName);
 
 
 
             });
         };
+
+        o.initShareButton = function() {
+            //google
+            (function() {
+                var po = document.createElement('script');
+                po.type = 'text/javascript';
+                po.async = true;
+                po.src = 'https://apis.google.com/js/plusone.js';
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(po, s);
+            })();
+
+            //facebook
+            (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s);
+                js.id = id;
+                js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&version=v2.0";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+
+            //twitter
+            ! function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (!d.getElementById(id)) {
+                    js = d.createElement(s);
+                    js.id = id;
+                    js.src = "https://platform.twitter.com/widgets.js";
+                    fjs.parentNode.insertBefore(js, fjs);
+                }
+            }(document, "script", "twitter-wjs");
+
+
+        }
+
 
         o.footerSelect = function(data) {
 
