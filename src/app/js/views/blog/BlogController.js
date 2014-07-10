4bc7059c5e6b6324c534f7b0f004c9f29084f734
Muhammad K Khan <mk.muhammad.khan10@gmail.com>
2014-07-10 12:17:33 -0500

    Modify: blog.jade file and the controller
    Fix read more link, it should now open in a new tab
    Removed the addPost function from the model to the controller.
       Everything should be working as before.
    Add comments to the addPost function.

--- src/app/js/views/blog/BlogController.js
+++ src/app/js/views/blog/BlogController.js
@@ -1,6 +1,6 @@
 define(["dojo/dom","dijit/registry", "modules/HashController", "esri/urlUtils",
-    "views/map/MapConfig", "modules/EventsController", "views/blog/BlogModel",  "dojo/Deferred"],
-    function(dom, registry, HashController, urlUtils, MapConfig, EventsController, BlogModel, Deferred) {
+    "views/map/MapConfig", "modules/EventsController", "views/blog/BlogModel",  "dojo/Deferred", "dojo/_base/array"],
+    function(dom, registry, HashController, urlUtils, MapConfig, EventsController, BlogModel, Deferred, array) {
 
         var o = {};
         var initialized = false;
@@ -60,7 +60,7 @@ define(["dojo/dom","dijit/registry", "modules/HashController", "esri/urlUtils",
 
                     var model = JSON.parse(res);
 
-                    BlogModel.addPosts(model);
+                    o.addPosts(model);
 
                     dom.byId(viewId).innerHTML = html;
                     EventsController.switchToView(viewObj);
@@ -92,6 +92,80 @@ define(["dojo/dom","dijit/registry", "modules/HashController", "esri/urlUtils",
 
             return deffered.promise;
         };
+
+        o.addPosts = function (Posts) {
+            // add arrays
+            console.log(Posts)
+            Posts['articles'].forEach(function (item) {
+                item.getAuthors = function () {
+                    var authors = this.author.split(",");
+
+                    var result = [];
+                    /**
+                     *  We are currently in a blog post. We want to add a function that will
+                     *  return all the authors in a nice array for us to print out to the view.
+                     */
+                    array.forEach(authors, function (item, index) {
+
+                        /**
+                         * Iterate over the authors that we now have in an array.
+                         * The if statement correspond to the following rules:
+                         * 1. if it is the first item in the array, take out the by
+                         *    line and add it as two separate eateries into the array.
+                         * 2. If there are only one author in the array, we have to everything in one go.
+                         *    Separate the by line, and the date.
+                         * 3. if we are -2 index away from the array, the last index holds the date and
+                         *    current index holds the first half of the date. Separate the two things out and
+                         *    add as two separate entities
+                         * 4. do nothing
+                         * 5. Just remove the empty white space and add the value to the array.
+                         * */
+
+                        if ( index == 0 && authors.length != 2 )
+                        { // remove by from the author and add the author to the list
+                            result.push("by");
+                            result.push(item.substring(3,item.length));
+                        }
+                        else if ( authors.length == 2 && index == 0 )
+                        {
+                            var a = item.split("-");
+
+                            result.push("by");
+                            result.push(a[0].substring(3,item.length));
+                            result.push(a[1].trim() + authors[index+1]);
+
+                        }
+                        else if ( index == authors.length-2)
+                        { // seperate the n-2 index of the array and make it 3, and concat the string.
+                            var a = authors[index].split("and");
+                            // the first item in the set is fine
+                            result.push(a[0].trim());
+                            result.push("and");
+
+                            // more logic
+                            var b = a[1].split("-")
+                            result.push(b[0].trim())
+                            result.push(b[1]+authors[index+1]);
+                        }
+                        else if ( index == authors.length-1)
+                        {
+                            // do nothing
+                        }
+                        else
+                        {
+                            result.push(item.trim());
+                        }
+                    })
+                    return result;
+                };
+            });
+
+            BlogModel.vm.blogPost.push(Posts);
+
+
+        }
+
+
 
         //listen to key
 
--- src/app/js/views/blog/BlogModel.js
+++ src/app/js/views/blog/BlogModel.js
@@ -1,5 +1,5 @@
-define(["knockout", "main/Config", "dojo/dom", "dojo/_base/array"],
-    function(ko, Config, dom, array) {
+define(["knockout", "main/Config", "dojo/dom"],
+    function(ko, Config, dom) {
 
         var o = {};
 
@@ -16,73 +16,9 @@ define(["knockout", "main/Config", "dojo/dom", "dojo/_base/array"],
             ko.applyBindings(vm, dom.byId(domId));
         }
 
-        o.addPosts = function (Posts) {
-            // add arrays
-            Posts['articles'].forEach(function (item) {
-                item.getAuthors = function () {
-                    var authors = this.author.split(",");
-
-                    var result = [];
-                    var cont = false;
-
-                    array.forEach(authors, function (item, index) {
-
-
-
-
-                       if ( index == 0 && authors.length != 2)
-                       { // remove by from the author and add the author to the list
-                           result.push("by");
-                           result.push(item.substring(3,item.length));
-                       }
-                       else if ( authors.length == 2 && index == 0 )
-                       {
-                           var a = item.split("-");
-
-                           result.push("by");
-                           result.push(a[0].substring(3,item.length));
-                           result.push(a[1].trim() + authors[index+1]);
-
-                       }
-                       else if ( index == authors.length-2)
-                       { // seperate the n-2 index of the array and make it 3, and concat the string.
-                           var a = authors[index].split("and");
-                           // the first item in the set is fine
-                           result.push(a[0].trim());
-                           result.push("and");
-
-                           // more logic
-                           var b = a[1].split("-")
-                           result.push(b[0].trim())
-                           result.push(b[1]+authors[index+1]);
-                       }
-                       else if ( index == authors.length-1)
-                       {
-                           // do nothing
-                       }
-                       else
-                       {
-                           result.push(item.trim());
-                       }
-                    })
-                    return result;
-                };
-            });
-
-            o.vm.blogPost.push(Posts);
-
-
-        }
-
         vm.getAllPost = function () {
-            console.log(o.vm.blogPost()[0]['articles']);
             return o.vm.blogPost()[0]['articles'];
         }
-
-        // TODO: each blog post should have a getAuthor functions that return the correct string for the authors. There are two ways to do this as of right now: 1. add it manually, or 2. ko.computed.
-        vm.getAuthors = function () {
-            console.log('Authors are :' + authors);
-        };
 
         return o;
 
