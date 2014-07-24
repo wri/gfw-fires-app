625a35ff27745befcecd614cbc1b3e620f2943f0
Robert Winterbottom <rwinterbottom@blueraster.com>
2014-07-24 12:57:21 -0400

    modified gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -181,7 +181,7 @@ module.exports = function(grunt) {
       default: {
         options: {
           host: 'staging.blueraster.com',
-          dest: 'html/wri/gfw-fires/v12/',
+          dest: 'html/wri/gfw-fires/v13/',
           username: '',
           password: ''
         },
