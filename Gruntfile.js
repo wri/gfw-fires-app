66220a8dd2ee4b2d20fe95e263c36f00c7837e25
Robert Winterbottom <rwinterbottom@blueraster.com>
2014-07-31 14:34:27 -0400

    updated gruntfile to point to new directory on staging

--- Gruntfile.js
+++ Gruntfile.js
@@ -181,7 +181,7 @@ module.exports = function(grunt) {
       default: {
         options: {
           host: 'staging.blueraster.com',
-          dest: 'html/wri/gfw-fires/v15/',
+          dest: 'html/wri/gfw-fires/v16/',
           username: '',
           password: ''
         },
