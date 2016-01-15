f9e4cd9adc4b7501e0e10d2a66cd5ca720080b27
Lucas Cotner <lcotner@blueraster.com>
2016-01-15 14:41:35 -0500

    Updated to latest version of the js api

--- www/src/app/bootloader.js
+++ www/src/app/bootloader.js
@@ -16,11 +16,11 @@
     var ENV = 'dev',
 
         version = "0.7.40",
-        esriLib = 'http://js.arcgis.com/3.13/init.js',
+        esriLib = 'http://js.arcgis.com/3.15/init.js',
 
         css = {
             'dev': [{
-                src: 'http://js.arcgis.com/3.13/esri/css/esri.css',
+                src: 'http://js.arcgis.com/3.15/esri/css/esri.css',
                 cdn: true
             }, {
                 src: 'app/css/app.css',
@@ -38,11 +38,11 @@
                 src: 'app/css/map.css',
                 cdn: false
             }, {
-                src: 'http://js.arcgis.com/3.13/dijit/themes/tundra/tundra.css',
+                src: 'http://js.arcgis.com/3.15/dijit/themes/tundra/tundra.css',
                 cdn: true
             }],
             'pro': [{
-                src: 'http://js.arcgis.com/3.13/esri/css/esri.css',
+                src: 'http://js.arcgis.com/3.15/esri/css/esri.css',
                 cdn: true
             }, {
                 src: 'app/css/app.css',
@@ -60,7 +60,7 @@
                 src: 'app/css/map.css',
                 cdn: false
             }, {
-                src: 'http://js.arcgis.com/3.13/dijit/themes/tundra/tundra.css',
+                src: 'http://js.arcgis.com/3.15/dijit/themes/tundra/tundra.css',
                 cdn: true
             }]
         },
