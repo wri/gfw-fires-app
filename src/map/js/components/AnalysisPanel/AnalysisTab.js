d28d65954aedcd39bcdec221a69ad59cc348d6d3
Lucas Cotner <lcotner@blueraster.com>
2016-03-01 17:25:27 -0500

    Wired up analyis with chosen library

--- src/map/js/components/AnalysisPanel/AnalysisTab.js
+++ src/map/js/components/AnalysisPanel/AnalysisTab.js
@@ -2,11 +2,16 @@ import {analysisPanelText} from 'js/config';
 import {analysisActions} from 'actions/AnalysisActions';
 import AnalysisComponent from 'components/LayerPanel/AnalysisComponent';
 import React from 'react';
+import Chosen from 'chosen';
 
 export default class AnalysisTab extends React.Component {
 
   constructor (props) {
     super(props);
+    this.state = {
+      activeIslands: [],
+      activeProvinces: []
+    };
   }
 
   componentDidMount () {
@@ -16,11 +21,26 @@ export default class AnalysisTab extends React.Component {
     calendar.subscribe('change', function (date) {
       console.debug(date);
     });
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+
+    if (prevProps.provinces.length === 0 && this.props.provinces.length > 0) {
+      $('#provinces').chosen();
+    } else if (prevProps.areaIslandsActive === true && this.props.areaIslandsActive === false) {
+      $('#islands').chosen('destroy');
+      $('#provinces').chosen();
+    } else if (prevProps.areaIslandsActive === false && this.props.areaIslandsActive === true) {
+      $('#provinces').chosen('destroy');
+      $('#islands').chosen();
+    }
   }
 
   render () {
     let className = 'text-center';
     if (this.props.activeTab !== analysisPanelText.analysisTabId) { className += ' hidden'; }
+
     return (
       <div className={className}>
         <p>{analysisPanelText.analysisAreaHeader}</p>
@@ -35,16 +55,22 @@ export default class AnalysisTab extends React.Component {
           </label>
         </div>
         <div className='padding'>
-          <select className={`fill__wide ${this.props.areaIslandsActive === true ? '' : 'hidden'}`} multiple onChange={this.change} disabled={this.props.islands.length === 0}>
+        {this.props.islands.length > 0 ?
+          <select multiple id='islands' className={`chosen-select-no-single fill__wide ${this.props.areaIslandsActive === true ? '' : 'hidden'}`} onChange={this.change} disabled={this.props.islands.length === 0}>
             {this.props.islands.map((i) => (
               <option value={i}>{i}</option>
             ))}
           </select>
-          <select className={`fill__wide ${this.props.areaIslandsActive === false ? '' : 'hidden'}`} multiple onChange={this.change} disabled={this.props.provinces.length === 0}>
+          : null
+        }
+        {this.props.islands.length > 0 ?
+          <select multiple id='provinces' className={`chosen-select-no-single fill__wide ${this.props.areaIslandsActive === false ? '' : 'hidden'}`} onChange={this.change}>
             {this.props.provinces.map((p) => (
               <option value={p}>{p}</option>
             ))}
           </select>
+          : null
+        }
         </div>
         <p>{analysisPanelText.analysisTimeframeHeader}</p>
         <AnalysisComponent {...this.state} options={analysisPanelText.analysisCalendar} />
