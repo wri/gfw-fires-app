cb8cf8f999aaa97e9163c8ea6c4c2f876ee260cd
Lucas Cotner <lcotner@blueraster.com>
2016-03-16 10:23:22 -0400

    Added a proper date picker for the Fire Risk Layer

---
+++ src/map/js/components/LayerPanel/RiskControls.js
@@ -0,0 +1,36 @@
+import {modalActions} from 'actions/ModalActions';
+import {mapStore} from 'stores/MapStore';
+import {mapActions} from 'actions/MapActions';
+import DateHelper from 'helpers/DateHelper';
+import React from 'react';
+
+export default class RiskControls extends React.Component {
+
+  constructor (props) {
+    super(props);
+    mapStore.listen(this.storeUpdated.bind(this));
+    this.state = mapStore.getState();
+  }
+
+  storeUpdated () {
+    this.setState(mapStore.getState());
+  }
+
+  render () {
+
+    let date = window.Kalendae.moment(this.state.riskDate);
+
+    return <div>
+      <div id='risk-date-ranges'>
+        <span className='imagery-calendar-label'>{this.props.options.label}</span>
+        <button className={`gfw-btn white pointer ${this.state.calendarVisible === 'riskStart' ? ' current' : ''}`} onClick={this.changeRisk.bind(this)}>{DateHelper.getDate(date)}</button>
+      </div>
+    </div>;
+  }
+
+  changeRisk() {
+    modalActions.showCalendarModal('start');
+    mapActions.setCalendar('risk');
+  }
+
+}
