8cdb885b8d55656a39326efff9f02c262d45cb14
Lucas Cotner <lc07@uw.edu>
2016-07-08 17:34:00 -0400

    Added a confirmation modal for alerts

---
+++ src/map/js/components/Modals/ConfirmationModal.js
@@ -0,0 +1,70 @@
+/* @flow */
+import ModalWrapper from 'components/Modals/ModalWrapper';
+import {modalStore} from 'stores/ModalStore';
+import {modalText} from 'js/config';
+import React, {
+  Component
+} from 'react';
+
+export default class ConfirmationModal extends Component {
+  displayName: ConfirmationModal;
+  state: any;
+
+  constructor(props: any) {
+    super(props);
+
+    modalStore.listen(this.storeUpdated.bind(this));
+    let defaultState = modalStore.getState();
+    this.state = {
+      confirmationInfo: defaultState.confirmationInfo
+    };
+  }
+
+  storeUpdated () {
+    let currentState = modalStore.getState();
+    this.setState({ confirmationInfo: currentState.confirmationInfo });
+  }
+
+  render () {
+
+    return (
+      <ModalWrapper>
+        <div>
+          <div className='canopy-modal-title'>Subscription success!</div>
+          {this.state.confirmationInfo.email ? <div className='longer'>{modalText.subscription.emailExplanation} </div> : null}
+          {this.state.confirmationInfo.phoneNumber ? <div className='longer'>{modalText.subscription.phoneExplanation} </div> : null}
+        </div>
+      </ModalWrapper>
+    );
+  }
+
+  tableMap (item: string, label: string) {
+    return (
+      <dl className='source-row'>
+        <dt>{label}</dt>
+        <dd dangerouslySetInnerHTML={{ __html: item }}></dd>
+      </dl>
+    );
+  }
+
+  summaryMap (item: any) {
+    if (typeof item === 'string') {
+      return <p dangerouslySetInnerHTML={{ __html: item }} />;
+    } else {
+      return (
+        <ul>
+          {item.map(listItem => <li dangerouslySetInnerHTML={{ __html: listItem }} />)}
+        </ul>
+      );
+    }
+  }
+
+  paragraphMap (item: string) {
+    return <p dangerouslySetInnerHTML={{ __html: item }} />;
+  }
+
+  htmlContentMap (item: string) {
+    return <div dangerouslySetInnerHTML={{ __html: item }} />;
+  }
+
+}
