16db3dbd1b1c29e8f9e6e87ec7f4d5d223ad0a0b
dainguyendo <dainguyendo@gmail.com>
2017-10-13 10:33:43 -0400

    Air quality layer
    New service for air quality - requiring changes to layer definitions and textual notes

--- src/map/js/config.js
+++ src/map/js/config.js
@@ -187,7 +187,7 @@ export const config = {
         date: new window.Kalendae.moment(),
         method: 'changeAirQ',
         direction: 'past',
-        startDate: new window.Kalendae.moment('09/25/2015'),
+        startDate: new window.Kalendae.moment('10/12/2017'),
         domId: 'airQ',
         domClass: 'airQ'
       },
@@ -730,16 +730,16 @@ export const config = {
       order: 25,
       type: 'dynamic',
       label: 'Air quality',
-      sublabel: '(layer starts at 9/25/15)',
+      sublabel: '(layer starts at 10/12/2017)',
       group: 'airQuality',
       className: 'air-quality',
-      url: 'https://gis-gfw.wri.org/arcgis/rest/services/Fires/AirQuality_sea/MapServer',
+      url: 'https://gfw-staging.wri.org/arcgis/rest/services/Fires/OpenAQI/MapServer',
       metadataId: 'air_quality_old',
       calendar: {
         domId: 'airQCalendar',
         domClass: 'air-quality'
       },
-      layerIds: [1]
+      layerIds: [0]
     },
     {
       id: KEYS.overlays,
