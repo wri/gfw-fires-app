ba6a84a6ea144afc94d67a521b32941161f80d7a
Lucas Cotner <lc07@uw.edu>
2018-07-31 08:15:40 -0400

    Cleaner planet initialization errback

--- src/map/js/main.js
+++ src/map/js/main.js
@@ -100,7 +100,7 @@ const parseQuarterlyTitle = (title) => {
   }
 };
 
-const newPromise = new Promise((resolve, reject) => {
+const queryPlanet = new Promise((resolve, reject) => {
   // Request XML page
   const xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
@@ -153,36 +153,24 @@ const newPromise = new Promise((resolve, reject) => {
   xhttp.send();
 });
 
-let initializeApp = () => {
-  newPromise.then((done) => {
-    if (done) {
-      app.debug('main >>> initializeApp');
-      ReactDOM.render(<Map />, document.getElementById('root'));
-      ReactDOM.render(<LayerModal />, document.getElementById('layer-modal'));
-      ReactDOM.render(<CanopyModal />, document.getElementById('canopy-modal'));
-      ReactDOM.render(<SearchModal />, document.getElementById('search-modal'));
-      ReactDOM.render(<BasemapModal />, document.getElementById('basemap-modal'));
-      ReactDOM.render(<CalendarModal calendars={defaults.calendars}/>, document.getElementById('calendar-modal'));
-      ReactDOM.render(<SubscriptionModal />, document.getElementById('subscription-modal'));
-      ReactDOM.render(<ConfirmationModal />, document.getElementById('confirmation-modal'));
-      ReactDOM.render(<FiresModal />, document.getElementById('fires-modal'));
-      ReactDOM.render(<ShareModal />, document.getElementById('share-modal'));
-    }
-  }, () => {
-    app.debug('main >>> initializeApp');
-    ReactDOM.render(<Map />, document.getElementById('root'));
-    ReactDOM.render(<LayerModal />, document.getElementById('layer-modal'));
-    ReactDOM.render(<CanopyModal />, document.getElementById('canopy-modal'));
-    ReactDOM.render(<SearchModal />, document.getElementById('search-modal'));
-    ReactDOM.render(<BasemapModal />, document.getElementById('basemap-modal'));
-    ReactDOM.render(<CalendarModal calendars={defaults.calendars}/>, document.getElementById('calendar-modal'));
-    ReactDOM.render(<SubscriptionModal />, document.getElementById('subscription-modal'));
-    ReactDOM.render(<ConfirmationModal />, document.getElementById('confirmation-modal'));
-    ReactDOM.render(<FiresModal />, document.getElementById('fires-modal'));
-    ReactDOM.render(<ShareModal />, document.getElementById('share-modal'));
-  });
+const initializeApp = () => {
+  app.debug('main >>> initializeApp');
+  ReactDOM.render(<Map />, document.getElementById('root'));
+  ReactDOM.render(<LayerModal />, document.getElementById('layer-modal'));
+  ReactDOM.render(<CanopyModal />, document.getElementById('canopy-modal'));
+  ReactDOM.render(<SearchModal />, document.getElementById('search-modal'));
+  ReactDOM.render(<BasemapModal />, document.getElementById('basemap-modal'));
+  ReactDOM.render(<CalendarModal calendars={defaults.calendars}/>, document.getElementById('calendar-modal'));
+  ReactDOM.render(<SubscriptionModal />, document.getElementById('subscription-modal'));
+  ReactDOM.render(<ConfirmationModal />, document.getElementById('confirmation-modal'));
+  ReactDOM.render(<FiresModal />, document.getElementById('fires-modal'));
+  ReactDOM.render(<ShareModal />, document.getElementById('share-modal'));
 };
 
 configureApp();
 lazyloadAssets();
-initializeApp();
+queryPlanet.then(() => {
+  initializeApp();
+}, () => {
+  initializeApp();
+});
