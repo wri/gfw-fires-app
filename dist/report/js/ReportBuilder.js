d4e73649cd4a252005c45934e512994e17bd546b
Anthony <acalderaro@blueraster.com>
2019-02-26 16:53:55 -0500

    Consolidated code to work for HTTP and HTTPS.

--- dist/report/js/ReportBuilder.js
+++ dist/report/js/ReportBuilder.js
@@ -734,10 +734,8 @@ define([
             const baseURI = fullURIArray[0];
             const hashString = encodeURIComponent('#' + fullURIArray[1]);
             const longURIParsed = baseURI + hashString;
-            console.log('https?', window.location.href.indexOf('https'))
-            // if (window.location.href.indexOf('https') === -1) 
-            $.getJSON("https://api-ssl.bit.ly/v3/shorten?login=gfwfires&apiKey=R_d64306e31d1c4ae489441b715ced7848&longUrl=" + longURIParsed, function (response) {
-              console.log('???', response);
+            
+            $.getJSON("https://api.bit.ly/v3/shorten?login=gfwfires&apiKey=R_d64306e31d1c4ae489441b715ced7848&longUrl=" + longURIParsed, function (response) {
               const bitlyShortLink = response.data.url;
               $('.share-link')
                 .on('click', function () {
