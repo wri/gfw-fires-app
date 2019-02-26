5fe5c80412bf65d1ab5667e72de614978d1129cf
Anthony <acalderaro@blueraster.com>
2019-02-26 10:09:32 -0500

    Updated package.json.:

--- dist/report/js/ReportBuilder.js
+++ dist/report/js/ReportBuilder.js
@@ -734,7 +734,8 @@ define([
             const baseURI = fullURIArray[0];
             const hashString = encodeURIComponent('#' + fullURIArray[1]);
             const longURIParsed = baseURI + hashString;
-            $.getJSON("https://api.bitly.com/v3/shorten?login=gfwfires&api_key=R_d64306e31d1c4ae489441b715ced7848&longUrl=" + longURIParsed, function (response) {
+            $.getJSON("http://api.bitly.com/v3/shorten?login=gfwfires&longUrl=" + longURIParsed, function (response) {
+              console.log('???');
               const bitlyShortLink = response.data.url;
               $('.share-link')
                 .on('click', function () {
