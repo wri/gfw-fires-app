25c64e5cdf87bee1dca3e843cb70ad9211bef644
Anthony <acalderaro@blueraster.com>
2019-02-26 15:39:50 -0500

    Dist.

--- dist/report/js/ReportBuilder.js
+++ dist/report/js/ReportBuilder.js
@@ -34,23 +34,7 @@ define([
 
       init: function() {
         var self = this;
-        // Add proxy rules
-        // console.log('???', urlUtils.getProxyRule('https://api.bit.ly/v3/shorten')); // undefined
-        urlUtils.addProxyRule({
-          urlPrefix: 'http://api.bitly.com/v3/shorten?login=gfwfires&longUrl=',
-          // proxyUrl: '../../map/php/proxy.php'
-          proxyUrl: 'http://fires-staging.globalforestwatch.org/map/php/proxy.php'
-          // https://fires-staging.globalforestwatch.org/map/php/proxy.php
-        });
-        
-        urlUtils.addProxyRule({
-          urlPrefix: 'http://api.bitly.com/v3/shorten?login=gfwfires&longUrl=',
-          // proxyUrl: '../../map/php/proxy.php'
-          proxyUrl: 'http://fires-staging.globalforestwatch.org/map/php/proxy.php'
-        });
-        
-        console.log('???', urlUtils.getProxyRule('https://api.bitly.com/v3/shorten?login=gfwfires&longUrl=')); // defined
-        console.log('???', urlUtils.getProxyRule('http://api.bitly.com/v3/shorten?login=gfwfires&longUrl=')); // defined
+
           self.init_report_options();
 
           this.getIdOne().then(() => {
@@ -743,27 +727,23 @@ define([
         },
 
         init_report_options: function() {
-          console.log('???');
             const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
             const self = this;
+            const bitlyShortLink = window.location.href;
+            const copyToClip = (element) => {
+              element.select();
+              document.execCommand("copy");
+            }
 
-            const fullURI = window.location.href;
-            const fullURIArray = fullURI.split("#");
-            const baseURI = fullURIArray[0];
-            const hashString = encodeURIComponent('#' + fullURIArray[1]);
-            const longURIParsed = baseURI + hashString;
-            $.getJSON("http://fires-staging.globalforestwatch.org/map/php/proxy.php?http://api.bitly.com/v3/shorten?login=gfwfires&longUrl=" + longURIParsed, function(response) {
-              console.log('response', response);
-              // const bitlyShortLink = response.data.url;
-              const bitlyShortLink = window.location.href;
-              $('.share-link')
-                .on('click', function () {
-                  console.log('!!!');
-                  document.querySelector('.share-link-input__container').classList.toggle("hidden");
-                  $('.share-link-input').val(bitlyShortLink);
-                });
-            });
+            $('.share-link')
+              .on('click', function () {
+                document.querySelector('.share-link-input__container').classList.toggle("hidden");
+                $('.share-link-input').val(bitlyShortLink);
+                let copyText = document.querySelector('.share-link-input');
+                copyToClip(copyText);
+                console.log('???');
+              });
 
             self.read_hash();
 
