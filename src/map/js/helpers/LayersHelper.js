d596896e3a2b4b2633bca04b250598df5ab221df
Anthony Calderaro <acalderaro@blueraster.com>
2020-06-02 11:16:23 -0400

    Added new date formatter for the updated wind suitability mapper archive in S3.

--- src/map/js/helpers/LayersHelper.js
+++ src/map/js/helpers/LayersHelper.js
@@ -1013,7 +1013,15 @@ let LayersHelper = {
 
     let reportdates = dateArray.split('/');
     let datesFormatted = reportdates[2].toString() + reportdates[0].toString() + reportdates[1].toString();
-    let updatedURL = 'https://suitability-mapper.s3.amazonaws.com/wind/archive/wind-surface-level-gfs-' + datesFormatted + '00' + '.1-0.gz.json';
+    const lastYearBeforeURLReformatting = 20200102; // On Jan 3, 2020, the format of the files in S3 was changed.
+    let updatedURL = 'https://suitability-mapper.s3.amazonaws.com/wind/archive/wind-surface-level-gfs-';
+    if (Number(datesFormatted) > lastYearBeforeURLReformatting) {
+      updatedURL += `${datesFormatted}00%2F.1-0.gz.json`;
+    } else {
+      updatedURL += `${datesFormatted}00.1-0.gz.json`;
+    }
+
+    console.log('======= updating!', updatedURL);
     WindHelper.deactivateWindLayer();
     WindHelper.activateWindLayer(updatedURL);
   },
